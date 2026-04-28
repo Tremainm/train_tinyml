61521451cff4dc4a1dd76f12a60b614819dd583d
g00367973@atu.ie <g00367973@atu.ie>
2026-04-28 09:49:40 +0100

    cleanup

--- train_tinyml_context.py
+++ train_tinyml_context.py
@@ -16,23 +16,31 @@ print("Class order (note this down for firmware):", list(le.classes_))
 
 X = df[["temperature", "humidity"]].values
 scaler = MinMaxScaler()
-X_scaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(X) # normalised to a range [0, 1]
 
 print("Scaler min_:", scaler.min_)
 print("Scaler scale_:", scaler.scale_)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=42, stratify=y)
+# 80/20 train/test split
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42, stratify=y)
 # stratify=y ensures each split has proportional class representation
 
 n_classes = len(le.classes_)  # 3
 
+# 2 layer network: input - temp + humid
+#                  8 neurons, ReLu activation (model can learn patterns), Every input is connected to each 8 neurons.
+#                  2 * 8 + 8 = 24 learnable weights
+#                  outputs - 3 neurons (Softmax), prob per class that sum to 1
 model = keras.Sequential([
     keras.Input(shape=(2,)),
     keras.layers.Dense(8, activation="relu"),
     keras.layers.Dense(n_classes, activation="softmax")
 ])
 
+# Adam (Adaptive Movement Estimation): updates model's weights after each batch. Auto adjusts learning rate per param.
+# loss: How wrong the predictions are. 
+#       "Sparse"                 - plain ints (0, 1, 2). 
+#       categorical_crossenrtopy - heavily penalises confident wrong predictions. Pushes model to assign high prob to correct class.
 model.compile(optimizer="adam",
               loss="sparse_categorical_crossentropy",
               metrics=["accuracy"])
@@ -58,6 +66,7 @@ converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
 converter.inference_input_type = tf.int8
 converter.inference_output_type = tf.int8
 
+# compatibility workaround for microcontroller runtimes that don't support per-channel quantization
 converter._experimental_disable_per_channel_quantization_for_dense_layers = True
 
 tflite_model = converter.convert()
